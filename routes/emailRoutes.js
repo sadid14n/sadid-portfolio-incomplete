b9bf70346f8c8590adec1cbd0e69e3b9c16d90cf
Sadid <sadid@Sadid.com>
2024-08-27 20:41:14 +0530

    first commit

--- routes/emailRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { sendEmail } = require("../controllers/emailController");
-
-router.post("/sendEmail", sendEmail);
-
-module.exports = router;
